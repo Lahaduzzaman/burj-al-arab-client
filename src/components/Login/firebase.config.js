c77c78704401b9ac683cd0119fd8074b86ffdbef
Lahaduzzaman <criclahad.2@gmail.com>
2021-03-29 01:29:45 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCDYLnWUKv-u8-dz0G-E-aO0bQGkzJ_Yc",
+    authDomain: "burj-al-arab-a9733.firebaseapp.com",
+    projectId: "burj-al-arab-a9733",
+    storageBucket: "burj-al-arab-a9733.appspot.com",
+    messagingSenderId: "277414008303",
+    appId: "1:277414008303:web:d4a5fe57d839b5177fd21a"
+  };
+
+  export default firebaseConfig;
